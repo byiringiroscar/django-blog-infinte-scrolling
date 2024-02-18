580f6bc6f1f0aa885beb7f345934efe66deecc07
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-02-18 11:42:06 +0200

    wip on strucuture

--- home/models.py
+++ home/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    photo_url = models.URLField()
+
+    def __str__(self):
+        return self.title
