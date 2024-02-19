08ac43453540995ff79683f11f28604080b62011
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-02-19 11:26:01 +0200

    Add search on blog

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('', articles, name='articles'),
+    path('search/results/', search_results_view, name='search_results_view'),
 ]
--- home/views.py
+++ home/views.py
@@ -17,3 +17,24 @@ def articles(request):
     page_obj = paginator.get_page(page_number)
 
     return render(request, 'articles.html', {'page_obj': page_obj})
+
+
+def search_results_view(request):
+    query = request.GET.get('search', '')
+    page_number = request.GET.get('page', 1)
+    all_articles = Article.objects.all()
+    if query:
+        articles = all_articles.filter(title__icontains=query)
+        paginator = Paginator(articles, 10)
+        page_obj = paginator.get_page(page_number)
+    else:
+        articles = all_articles
+        paginator = Paginator(articles, 10)
+        page_obj = paginator.get_page(page_number)
+
+    context = {
+        'page_obj': page_obj,
+        'count': paginator.count,
+    }
+
+    return render(request, 'search_results.html', context)
